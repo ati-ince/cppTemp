1c96fd4902601748d41b2ff28a89250c76cef67c
ati ince <ati_mac@atis-MacBook-Pro.local>
2022-10-09 19:47:45 +0200

    analyze

---
+++ build_debug/CMakeFiles/tahir.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tahir.dir/src/main.cpp.o"
+  "CMakeFiles/tahir.dir/src/main.cpp.o.d"
+  "tahir"
+  "tahir.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tahir.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int, char **)
+{
+    int i ;
+    std::cout << "Hello\n";
+
+    std::cin >> i;
+
+    std::cout <<  i << "val";
+}
